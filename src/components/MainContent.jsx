30c5e385e22f29c991ea6a4cb30988ba347fa0eb
Arnav Panchal <arnavpanchal27@gmail.com>
2024-01-29 19:07:32 +0530

    Feat - color changes

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,13 +3,12 @@
 //import { useState } from 'react';
 import './MainContent.css'; // Import the CSS file for styling
 
+
 const MainContent = () => {
    // Initial width, you can change it as needed
 
   return (
     <div className="main-content">
-      {/* Your content goes here */}
-      <p>This is your main content component.</p>
       
     </div>
   );
